066619efff2928ed471d968e56c9a361ebdeabf9
danilobarros3 <danilo.barros3@outlook.com>
2026-05-06 20:33:44 -0300

    feat: init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { Geist_Mono, Inter } from "next/font/google"
+
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+import "./globals.css"
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <p>This is a test</p>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
