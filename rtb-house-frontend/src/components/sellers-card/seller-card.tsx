4913190b8baf6515a2116af3f279177523261669
danilobarros3 <danilo.barros3@outlook.com>
2026-05-07 16:08:08 -0300

    fix: style card sellers, add card shadcn lib

--- rtb-house-frontend/src/components/sellers-card/seller-card.tsx
+++ rtb-house-frontend/src/components/sellers-card/seller-card.tsx
@@ -1,5 +1,6 @@
 import { Seller } from "@/types/sellers.type";
 import { formatCurrencyUSD } from "@/utils/masks";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 type SellerCardProps = {
   seller: Seller;
@@ -7,14 +8,16 @@ type SellerCardProps = {
 
 export function SellerCard({ seller }: SellerCardProps) {
   return (
-    <article className="rounded-lg border bg-white p-4 shadow-sm">
-      <p className="text-lg font-medium text-black">
-        #{seller?.id}
-      </p>
-      <p className="mt-2 text-2xl font-semibold text-black">{seller?.name}</p>
-      <p className="text-xl font-medium text-red-500">
-        Total price: {formatCurrencyUSD(seller?.totalPrice || 0)}
-      </p>
-    </article>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg font-medium text-black">#{seller?.id}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-2xl font-semibold text-black">{seller?.name}</p>
+        <p className="text-xl font-medium text-red-500">
+          Total price: {formatCurrencyUSD(seller?.totalPrice || 0)}
+        </p>
+      </CardContent>
+    </Card>
   );
 }
