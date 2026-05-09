14a7d501d95729474036c275f5add1740ce4a5dc
danilobarros3 <danilo.barros3@outlook.com>
2026-05-09 11:29:48 -0300

    fix: remove id of seller in seller card component

--- rtb-house-frontend/src/components/sellers-card/seller-card.tsx
+++ rtb-house-frontend/src/components/sellers-card/seller-card.tsx
@@ -1,19 +1,16 @@
 import { Seller } from "@/types/sellers.type";
 import { formatCurrencyUSD } from "@/utils/masks";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 type SellerCardProps = {
-  seller: Seller;
+  seller: Seller; 
 };
 
 export function SellerCard({ seller }: SellerCardProps) {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle className="text-lg font-medium text-black">#{seller?.id}</CardTitle>
-      </CardHeader>
       <CardContent>
-        <p className="text-2xl font-semibold text-black">{seller?.name}</p>
+        <p className="text-2xl font-semibold text-black">Total of {seller?.name}</p>
         <p className="text-xl font-medium text-red-500">
           Total price: {formatCurrencyUSD(seller?.totalPrice || 0)}
         </p>
