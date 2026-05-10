06d025c9d6311790fb225faac671f292cc7b150c
danilobarros3 <danilo.barros3@outlook.com>
2026-05-10 15:55:33 -0300

    fix: style not found page and add button go to main page

--- rtb-house-frontend/src/app/not-found.tsx
+++ rtb-house-frontend/src/app/not-found.tsx
@@ -1,6 +1,9 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-gray-200">
+    <div className="flex flex-1 items-center justify-center bg-gray-200">
       <div className="text-center">
         <h1 className="text-8xl font-black text-black md:text-9xl">
           404
@@ -9,7 +12,14 @@ export default function NotFound() {
         <p className="mt-4 text-2xl font-semibold uppercase tracking-[0.3em] text-red-500">
           Not Found
         </p>
+        <p className="mt-4 text-base text-gray-500">  The page you were looking for does not exist.
+        </p>
+        <div className="flex items-center justify-center">
+        <Link href="/orders" className="mt-4 text-base text-gray-500 hover:text-red-500 flex items-center gap-2 border border-red-500 rounded-md px-4 py-2">
+          Go to main page <ArrowRight className="h-4 w-4" />
+        </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
